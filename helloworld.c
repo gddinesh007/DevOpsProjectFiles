c36a63bd4e6357ddb9fad424e602f75dd30272a7
Ubuntu <gddinesh007@gmail.com>
2020-04-21 20:21:25 +0000

    Commit the new files to GitHub repo

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+printf(“Hello Wolrd! Stay Home Stay Safe!”);
+
+return 0;
+
+}
+
+
---
+++ helloworld.java
@@ -0,0 +1,11 @@
+class helloworld {
+
+public static void main(String args[]){
+
+System.out.Println(“hello java world. stay home stay safe”);
+
+}
+
+}
+
+
